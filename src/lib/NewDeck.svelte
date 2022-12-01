6596b8fb94e03eb736952cc013dc672e9c1395ab
Nguyen Tran <nguyentran1072e@gmail.com>
2022-11-30 22:38:55 -0500

    Create a New Deck screen

---
+++ src/lib/NewDeck.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let deckName: string = "";
+  let deckDescription: string = "";
+
+  const createDeck = () => {
+    deckName = "";
+    deckDescription = "";
+  };
+
+  const cancelDeckCreation = () => {};
+</script>
+
+<h3>New Deck</h3>
+<input type="text" bind:value={deckName} />
+<textarea bind:value={deckDescription} />
+<button on:click={createDeck}>Create</button>
+<button on:click={cancelDeckCreation}>Cancel</button>
