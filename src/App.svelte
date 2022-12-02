34feb4a2b645f80615d0e524f971eae2d60a8d50
Nguyen Tran <nguyentran1072e@gmail.com>
2022-12-01 22:49:58 -0500

    Isolated NewDeck screen component

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,8 @@
 <script lang="ts">
   import NewDeck from "./lib/NewDeck.svelte";
-
-  let home = true;
-  let newDeckName = "";
-  let decks: string[] = [];
 </script>
 
-{#if home}
-  <button
-    on:click={() => {
-      home = false;
-    }}>New Deck</button
-  >
-  {#each decks as deck}
-    <p>{deck}</p>
-  {/each}
-  {#if newDeckName !== ""}
-    <p>Deck {newDeckName} is created.</p>
-  {/if}
-{:else}
-  <NewDeck
-    on:deckcreate={(event) => {
-      home = true;
-      newDeckName = event.detail.deckName;
-      decks.push(newDeckName);
-      decks = decks;
-      setTimeout(() => {
-        newDeckName = "";
-      }, 2000);
-    }}
-    on:deckcancel={() => {
-      home = true;
-    }}
-  />
-{/if}
+<NewDeck />
 
 <style>
 </style>
