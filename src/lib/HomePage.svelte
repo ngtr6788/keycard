218e554cf0974ff71998ad70d793002f1bbaceaa
Nguyen Tran <nguyentran1072e@gmail.com>
2022-12-17 23:28:53 -0500

    Connect home page to the create and get deck Rust backend

--- src/lib/HomePage.svelte
+++ src/lib/HomePage.svelte
@@ -1,29 +1,15 @@
 <script lang="ts">
   import { link, navigate } from "svelte-routing";
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  type Deck = {
+    id: number;
+    deck_name: string;
+    deck_description: string;
+  };
 
   // Temporary deck list
-  let deckNames = [
-    {
-      name: "VSCode Keyboard Shortcuts",
-      description: "Shortcuts I used in VSCode",
-      numberOfCards: 20,
-    },
-    {
-      name: "Vim Commands",
-      description: "Vim commands are not that easy to learn",
-      numberOfCards: 25,
-    },
-    {
-      name: "Emacs Commands made by (who?)",
-      description: "",
-      numberOfCards: 0,
-    },
-    {
-      name: "One Card only",
-      description: "Read the title",
-      numberOfCards: 1,
-    },
-  ];
+  let deckNamesPromise: Promise<Deck[]> = invoke("get_decks");
 </script>
 
 <div class="mx-10 my-4">
@@ -35,27 +21,36 @@
       use:link>New Deck</a
     >
   </div>
-  {#if deckNames.length !== 0}
-    <div class="grid grid-cols-2 my-1">
-      {#each deckNames as { name, description, numberOfCards }}
-        <div
-          class="border-2 border-black my-2 p-2 rounded-md hover:bg-slate-100 hover:shadow-lg hover:cursor-pointer odd:mr-2 even:ml-2"
-          on:keyup={() => navigate("/card-list")}
-          on:click={() => navigate("/card-list")}
-        >
-          <h5 class="text-m">{name}</h5>
-          <p
-            class={description ? "text-black text-sm" : "text-gray-500 text-sm"}
+  {#await deckNamesPromise}
+    <h4 class="text-base text-gray-500 my-1">Loading decks...</h4>
+  {:then deckNames}
+    {#if deckNames.length !== 0}
+      <div class="grid grid-cols-2 my-1">
+        {#each deckNames as { deck_name, deck_description }}
+          <div
+            class="border-2 border-black my-2 p-2 rounded-md hover:bg-slate-100 hover:shadow-lg hover:cursor-pointer odd:mr-2 even:ml-2"
+            on:keyup={() => navigate("/card-list")}
+            on:click={() => navigate("/card-list")}
           >
-            {description || "No description"}
-          </p>
-          <p class="text-sm">{numberOfCards} cards</p>
-        </div>
-      {/each}
-    </div>
-  {:else}
+            <h5 class="text-m">{deck_name}</h5>
+            <p
+              class={deck_description
+                ? "text-black text-sm"
+                : "text-gray-500 text-sm"}
+            >
+              {deck_description || "No description"}
+            </p>
+          </div>
+        {/each}
+      </div>
+    {:else}
+      <h4 class="text-base text-gray-500 my-1">
+        No decks here. Click "New Deck" to create a new deck.
+      </h4>
+    {/if}
+  {:catch}
     <h4 class="text-base text-gray-500 my-1">
-      No decks here. Click "New Deck" to create a new deck.
+      Cannot load decks right now. Try closing this window and try again.
     </h4>
-  {/if}
+  {/await}
 </div>
